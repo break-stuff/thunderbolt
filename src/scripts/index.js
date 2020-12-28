c501fa2026f2dabd7473de9b1e1faebf7cf36d1d
Burton Smith <burton@breakstuff.io>
2020-12-28 10:31:01 -0500

    update for accessibility

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@
     let lastScrollTop = 0;
     window.addEventListener("scroll", function () {
             let position = window.pageYOffset || document.documentElement.scrollTop;
-            position > lastScrollTop ? $bottomNav.classList.add("hide-nav") : $bottomNav.classList.remove("hide-nav");
+            position > lastScrollTop ? $bottomNav.setAttribute('aria-expanded', false) : $bottomNav.setAttribute('aria-expanded', true);
             lastScrollTop = position <= 0 ? 0 : position;
         }, { passive: true }
     );
